6af585cf293456e3e240016ca025229225f7c20b
spirina-a-t <spirinatest@gmail.com>
2020-12-08 17:14:14 +0300

    3.3. Разбор первой программы. [#426653]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Tanya Spirina");
+        System.out.println("05.12.2020");
+    }
+}
