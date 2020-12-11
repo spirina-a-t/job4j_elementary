141b286bc4e408a8e4fc86b9b59c8122186eec64
spirina-a-t <spirinatest@gmail.com>
2020-12-11 13:49:23 +0300

    5. Конвертер валюты [#426632] Исправлены стилистические ошибки, дописаны тесты для задания 0. Что такое тестирование [#426665]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,20 +2,32 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static int rubleToEuro(int value){
-        int rsl = value / 70; //формула перевода рублей в евро
-        return rsl;
+    public static int rubleToEuro(int value) {
+        return value / 70;
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = value/60; //формула перевода рублей в доллары
-        return rsl;
+        return value / 60;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int euro = Converter.rubleToEuro(140);
-        int dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + euro + " euro.");
+
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        int dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollar.");
+
+        int in1 = 180;
+        int expected1 = 3;
+        int out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("180 rubles are 3. Test result : " + passed1);
+
     }
 }
