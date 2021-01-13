14461e939f7548fe5b7f27ee2850e1af5934d480
spirina-a-t <[tatyananavis@gmail.com]>
2021-01-13 12:15:18 +0300

    5.5. Простое число [#426693] Удалены избыточные переменные.

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,16 +1,13 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
+
     public static boolean check(int number) {
-        int num;
-        boolean result = number >= 2;
+        boolean result = number > 1;
         for (int i = 2; i < number; i++) {
-            num = i;
-            if (number % num == 0) {
+            if (number % i == 0) {
                 result = false;
                 break;
-            } else {
-                result = true;
             }
         }
         return result;
