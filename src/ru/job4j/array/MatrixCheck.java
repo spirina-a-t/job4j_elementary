96b7f68d3ae3b917f2ed94c64c617547ce9c8ce6
spirina-a-t <[tatyananavis@gmail.com]>
2021-01-21 00:50:05 +0300

    6.7.4. Выигрышные комбинации в сокобан [#426604]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,16 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
 }
